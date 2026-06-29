180513a76f2f09a372f3458aade8959c675160ce
Monica Serrano <monicasesal@gmail.com>
2026-06-30 00:42:34 +0200

    Simular base de datos para desarrollar la lógica de la app

--- app.py
+++ app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 # Ruta principal
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    citas_veterinaria = [
+        {"hora": "10:00", "paciente": "Toby 🐶", "motivo": "Vacuna"},
+        {"hora": "11:30", "paciente": "Luna 🐱", "motivo": "Revisión"},
+        {"hora": "16:00", "paciente": "Thor 🐹", "motivo": "Corte de uñas"}
+    ]
+
+    chats_activos = [
+        {"usuario": "Carlos (Dueño de Rocky)", "mensaje": "Mi perro no quiere comer hoy", "atendido_por": "IA"},
+        {"usuario": "María (Dueña de Michi)", "mensaje": "¡Es una emergencia! Se ha tragado un hilo", "atendido_por": "Humano"},
+        {"usuario": "Ana (Dueña de Piolín)", "mensaje": "Muchas gracias por la cita", "atendido_por": "IA"}
+    ]
+    return render_template("index.html", mis_citas=citas_veterinaria, los_chats=chats_activos)
 
 # Arrancador del server
 if __name__ == "__main__":
