8669a0726a905abaddc7df973fb1784acb784b6b
Monica Serrano <monicasesal@gmail.com>
2026-07-02 16:00:11 +0200

    Conectar rutas de webhook

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from routes.web import web_blueprint
+from routes.webhook import webhook_blueprint
 
 app = Flask(__name__)
 
 # Registrar las rutas en la app
 app.register_blueprint(web_blueprint)
+app.register_blueprint(webhook_blueprint)
 
 # Arrancador del server
 if __name__ == "__main__":
